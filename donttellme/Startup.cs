8adbec236dbecbcbc4cfda8f5da06f2e530cd19a
Erik A. Brandstadmoen <erik@zammut.no>
2020-08-17 08:31:47 +0200

    Output caller IP address

---
+++ donttellme/Startup.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace donttellme
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context => { await context.Response.WriteAsync(GetRemoteIp(context)); });
+            });
+        }
+        
+        private static string GetRemoteIp(HttpContext context)
+        {
+            return context.Request.Headers["X-Forwarded-For"].FirstOrDefault()?
+                       .Split(":").FirstOrDefault()?
+                       .Split(",").FirstOrDefault()
+                   ?? context.Connection.RemoteIpAddress?.ToString();
+        }
+    }
+}
